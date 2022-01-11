4b6c54b93e353090ac763a457ae77088382b11ae
ele <ele-me@outlook.com>
2022-01-11 12:50:14 +0800

    fix shell

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -s -L https://github.com/jgm/pandoc/releases/download/2.9.2.1/pandoc-2.16.2-linux-amd64.tar.gz | tar xvzf - -C ./
+curl -s -L https://github.com/jgm/pandoc/releases/download/2.16.2/pandoc-2.16.2-linux-amd64.tar.gz | tar xvzf - -C ./
 # npm i -g hexo-cli gulp gulp-uglify
 # npm i
 export PATH="$(pwd)/pandoc-2.16.2/bin:$PATH"
