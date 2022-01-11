bc1e59947b5bac18dac270570fe78f0eaaa3a988
ele <ele-me@outlook.com>
2022-01-11 16:28:13 +0800

    clean

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl -s -L https://github.com/jgm/pandoc/releases/download/2.16.2/pandoc-2.16.2-linux-amd64.tar.gz | tar xvzf - -C ./
-# npm i -g hexo-cli gulp gulp-uglify
-# npm i
-
-export PATH="$(pwd)/pandoc-2.16.2/bin:$PATH"
-hexo generate && gulp build
