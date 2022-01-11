57deaf5e092623291b90b94edf04954958e4a45b
ele <ele-me@outlook.com>
2022-01-11 14:52:34 +0800

    hexo-cli

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
 curl -s -L https://github.com/jgm/pandoc/releases/download/2.16.2/pandoc-2.16.2-linux-amd64.tar.gz | tar xvzf - -C ./
 # npm i -g hexo-cli gulp gulp-uglify
 # npm i
+npm install hexo-cli -g
 export PATH="$(pwd)/pandoc-2.16.2/bin:$PATH"
 hexo generate && gulp build
